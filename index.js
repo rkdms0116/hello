fdc325c662a2be6c7837724b041f94e20734eeb6
rkdms <rkdms0116@gmail.com>
2023-12-04 11:52:35 +0900

    second

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 var os = require('os');
 var port = 8080;
 
+
 http.createServer( (req, res) => {
   console.log('Requested: ' + req.url);
   res.writeHead(200, { 'Content-Type':'text/html; charset=utf-8' });
